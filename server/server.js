14084e12850ceef78426c4ce124cb01e67e620ae
Lauren Buchholz <90865074+L-Buchholz@users.noreply.github.com>
2022-01-23 13:25:09 -0700

    Updated server.js
    Still working on routes to deploy to Heroku due to this assignment being broken from the outset

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// NOTE: express.static paths relative to process.cwd
+app.use(express.static("./client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+// while require() paths relative to the file
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
